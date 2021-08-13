168869ef12f9f5d3fccf8e80f6a3645121aa4d97
Mike Dacanay <migd64@gmail.com>
2021-08-13 16:02:33 -0700

    removed_ingredient worker bug fixed

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -25,11 +25,11 @@ const reducer = ( state = initialState, action ) => {
             }
             return updateObject(state, updatedState);
         case actionTypes.REMOVE_INGREDIENT:
-            const updatedIng = {[action.ingredientName]: state.ingredients[action.ingredientName] + 1}
+            const updatedIng = {[action.ingredientName]: state.ingredients[action.ingredientName] - 1}
             const updatedIngs = updateObject(state.ingredients, updatedIng);    
             const updatedSte = {
                 ingredients: updatedIngs,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName] 
+                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName] 
             }
             return updateObject(state, updatedSte);
         case actionTypes.SET_INGREDIENTS:
